d5fa56645d0f1b87c6cba41d467c30f3979f770d
Asrumochan <asrumochanparida99@gmail.com>
2024-01-10 00:16:30 +0530

    asd

--- tic_tac_toe/main.js
+++ tic_tac_toe/main.js
@@ -18,14 +18,14 @@ const winningPattern=[
     [0,4,8],
     [2,4,6] 
 ];
-let player1=prompt("Name of Player1 :")
-let player2=prompt("Name of Player2 :")
-if(player1===player2){
-    alert('Players name cannot be the same')
-    player2=prompt("Name of Player2 :")
-}
-p1.innerText=`${player1} is- X`
-p2.innerText=`${player2} is- O`
+// let player1=prompt("Name of Player1 :")
+// let player2=prompt("Name of Player2 :")
+// if(player1===player2){
+//     alert('Players name cannot be the same')
+//     player2=prompt("Name of Player2 :")
+// }
+// p1.innerText=`${player1} is- X`
+// p2.innerText=`${player2} is- O`
 
 boxes.forEach((box) => {
     box.addEventListener("click",()=>{
