a4cc6cdfe2f039f0774675ad47aa18cbe2ebe54e
Asrumochan <asrumochanparida99@gmail.com>
2024-01-09 12:01:26 +0530

    das

--- form/main.js
+++ form/main.js
@@ -5,13 +5,16 @@ const server = http.createServer((req,resp)=>{
     if (req.url==="/index") {
         fs.readFile("views/index.html", 'utf-8',(err,data)=>{
             if(err) throw err 
+
+            //resp.writeHead(200,'OK','UTF-8');
+            resp.setHeader("Content-Type", "text/html");
             resp.end(data)
         })
     }
 })
 server.listen(8182,"127.0.0.1",(err)=>{
     if(err) throw err 
-    console.log(`Server running on : http://${"127.0.0.1"}:${8182}`)
+    console.log(`Server running on : http://${"127.0.0.1"}:${8182}/index`)
 })
 
-const submit=window.document.getElementById('submit')
+// const submit=Document.getElementById('submit')
---
+++ form/views/test.js
@@ -0,0 +1,10 @@
+const sub =document.getElementById('sub');
+const name1= document.getElementById("name1");
+const email= document.getElementById("email");
+const password= document.getElementById("pass");
+let nm,pa;
+sub.addEventListener("click",()=>{
+     console.log(name1.value)
+     console.log(email.value)
+     console.log(password.value)
+})
