0f5bdc1ad2d724d7acb0c55162ceb406e4b2833e
Asrumochan <asrumochanparida99@gmail.com>
2024-01-09 20:06:15 +0530

    fdgh

--- form/views/test.js
+++ form/views/test.js
@@ -2,9 +2,19 @@ const sub =document.getElementById('sub');
 const name1= document.getElementById("name1");
 const email= document.getElementById("email");
 const password= document.getElementById("pass");
-let nm,pa;
 sub.addEventListener("click",()=>{
-     console.log(name1.value)
-     console.log(email.value)
-     console.log(password.value)
+     localStorage.setItem('name', name1.value)
+     localStorage.setItem('password', password.value)
+     localStorage.setItem('email', email.value)
 })
+
+function handler(){
+     const btn=document.getElementById("btn")
+     let nm = localStorage.getItem('name');
+     let pa = localStorage.getItem('password');
+     let em = localStorage.getItem('email');
+     if (btn.textContent===pa) {
+          alert("Password matched");
+          
+     }
+}
